f0f82c6a2b4dcb4877b64f2b6cd2741573aebdd1
David Björklund <david.bjorklund@gmail.com>
2019-05-12 15:47:17 +0200

    make assign nice w regards to types

--- lib/assign.ts
+++ lib/assign.ts
@@ -1,7 +1,18 @@
+import { Merge } from 'type-fest';
+
 import { set } from './set';
 
-export const assign = (input: object, ...args: object[]) => {
+type ResultType<T,U,V,W> = Merge<T, Merge<U, Merge<V, W>>>;
+
+export const assign = <T, U extends object, V extends object, W extends object>(
+  input: T,
+  arg1?: U,
+  arg2?: V,
+  arg3?: W
+): ResultType<T,U,V,W> => {
   const changes = {};
+
+  const args = [arg1, arg2, arg3].filter(Boolean);
 
   args.forEach((obj) => {
     for (const key in obj) {
@@ -9,7 +20,7 @@ export const assign = (input: object, ...args: object[]) => {
     }
   });
 
-  let result = input;
+  let result: any = input;
 
   for (const key in changes) {
     result = set(result, key, changes[key]);
