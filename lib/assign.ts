91ab5d771a8aa57e9e761a4ddb8cfd3a4aac9195
David Björklund <david.bjorklund@gmail.com>
2019-05-12 21:47:40 +0200

    more ts, more tests w assign()

--- lib/assign.ts
+++ lib/assign.ts
@@ -2,29 +2,41 @@ import { Merge } from 'type-fest';
 
 import { set } from './set';
 
-type ResultType<T,U,V,W> = Merge<T, Merge<U, Merge<V, W>>>;
+type ArgType = Record<keyof any, any>;
 
-export const assign = <T, U extends object, V extends object, W extends object>(
-  input: T,
-  arg1?: U,
-  arg2?: V,
-  arg3?: W
-): ResultType<T,U,V,W> => {
-  const changes = {};
+class Wrapper {
+  static assign<T>(input: Readonly<T>): T;
+  static assign<T, U extends ArgType>(
+    input: Readonly<T>,
+    arg: Readonly<U>,
+  ): Merge<T, U>;
+  static assign<T, U extends ArgType, V extends ArgType>(
+    input: Readonly<T>,
+    args1: Readonly<U>,
+    args2: Readonly<V>,
+  ): Merge<T, Merge<U, V>>;
+  static assign<T, U extends ArgType, V extends ArgType, W extends ArgType>(
+    input: Readonly<T>,
+    args1: Readonly<U>,
+    args2: Readonly<V>,
+    args3: Readonly<W>,
+  ): Merge<T, Merge<U, Merge<V, W>>>;
+  static assign(input: any, ...args: any[]) {
+    const changes: any = {};
+    args.forEach((obj) => {
+      for (const key in obj) {
+        changes[key] = obj[key];
+      }
+    });
 
-  const args = [arg1, arg2, arg3].filter(Boolean);
+    let result: any = input;
 
-  args.forEach((obj) => {
-    for (const key in obj) {
-      changes[key] = obj[key];
+    for (const key in changes) {
+      result = set(result, key, changes[key]);
     }
-  });
 
-  let result: any = input;
-
-  for (const key in changes) {
-    result = set(result, key, changes[key]);
+    return result;
   }
+}
 
-  return result;
-};
+export const assign = Wrapper.assign;
